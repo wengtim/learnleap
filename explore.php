e0f43c54f7fdf62ad5c5e3f7ed4c99a57d717e24
wengtim <wengtim05@proton.me>
2024-12-16 15:43:58 +0800

    feat: explore page done?

---
+++ explore.php
@@ -0,0 +1,52 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Explore</title>
+
+      <link rel="stylesheet" href="styles.css">
+      <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/remixicon@4.5.0/fonts/remixicon.css">
+      <style>
+      @import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap');
+      </style>
+   </head>
+
+   <body>
+      <header>
+         <a href="" class="logo">Learn<span>leap</a>
+         <ul class="navbar">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#explore">Explore</a></li>
+            <li><a href="#private">Join Private Quiz</a></li>
+            <li><a href="#contact">Contact</a></li>
+         </ul>
+
+         <div class="h-right">
+            <input type="text" placeholder="Search">
+         </div>
+      </header>
+      <div class="explore-content">
+         <h1>Most Played</h1>
+         <div class="explore-cards">
+            <div class="mostPlayed">
+               <button class="button">Button 1</button>
+               <button class="button">Button 2</button>
+               <button class="button">Button 3</button>
+            </div>
+
+         <h1>Most Liked</h1>
+            <div class="mostLiked">
+               <button class="button">Button 1</button>
+               <button class="button">Button 2</button>
+               <button class="button">Button 3</button>
+            </div>
+         </div>
+      </div>
+   </body>
+</html>
